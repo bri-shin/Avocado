4b6e03d69bda30e456d8bd42efba15b64dc1c5a1
Abdullah Zameek <arz268@nyu.edu>
2019-09-28 15:06:23 -0400

    added skeleton for backend

---
+++ backEnd/app.py
@@ -0,0 +1,38 @@
+'''
+Backend server for Avocado - HackNY 2019 
+'''
+
+########### RESTAURANT FUNCTIONS ##############
+
+
+
+
+
+########### END OF RESTAURANT FUNCTIONS ###########
+
+
+
+
+########## CHEF FUCTIONS #######################
+
+
+
+########### END OF CHEF FUNCTIONS ###########
+
+
+
+########## FOODIE FUCTIONS #######################
+
+########### END OF FOODIE FUNCTIONS ###########
+
+
+
+########## GOOGLE MAPS FUNCTIONS #######################
+
+########### END OF GOOGLE MAPS FUNCTIONS ###########
+
+
+
+########## MISC FUCTIONS #######################
+
+########### END OF MISC FUNCTIONS ###########
