75b491beb61e2d4b3149a134b7d42df8b061aca5
Brian Shin <bsshinple@gmail.com>
2019-09-28 16:01:35 -0400

    starting up

--- backEnd/app.py
+++ backEnd/app.py
@@ -2,23 +2,38 @@
 Backend server for Avocado - HackNY 2019 
 '''
 
+# Flask Imports
+from flask
+from flask import request
+
+# Firebase Imports
+import firebase_admin
+from firebase_admin import db
+from firebase_admin import credentials
+
+# Python Import
+import requests
+import json
+
+# Starting Up
+
+app = flask.Flask(__name__)
+cred = credentials.Certificate(
+    "seec-pennapps-firebase-adminsdk-7zh6f-41c2a963c2.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://seec-pennapps.firebaseio.com/'
+})
+
 ########### RESTAURANT FUNCTIONS ##############
-
-
-
 
 
 ########### END OF RESTAURANT FUNCTIONS ###########
 
 
-
-
 ########## CHEF FUCTIONS #######################
 
 
-
 ########### END OF CHEF FUNCTIONS ###########
-
 
 
 ########## FOODIE FUCTIONS #######################
@@ -26,11 +41,9 @@ Backend server for Avocado - HackNY 2019
 ########### END OF FOODIE FUNCTIONS ###########
 
 
-
 ########## GOOGLE MAPS FUNCTIONS #######################
 
 ########### END OF GOOGLE MAPS FUNCTIONS ###########
-
 
 
 ########## MISC FUCTIONS #######################
