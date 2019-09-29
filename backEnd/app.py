ac643a64a9ccd54f4455c32f783d48de9157fd80
Jung Soo <jsh562@nyu.edu>
2019-09-28 23:15:48 -0400

    get all restaurants

--- backEnd/app.py
+++ backEnd/app.py
@@ -43,6 +43,18 @@ def getRestaurantData():
         print(value)
     return(value)
 
+@app.route("/get-all-restaurants", methods=['GET'])
+def getAllRestaurants():
+    rest_list = RESTAURANTS.get()
+    rlist= []
+    for key in rest_list:
+        rlist.append(rest_list[key])
+    rdict = {
+        '0': rlist
+    }
+
+    return rdict
+
 @app.route("/create-restaurant", methods=['POST'])
 def createRestaurant():
     RestID = request.json['RestID']
@@ -93,13 +105,20 @@ def getFoodieData():
         print(value)
     return(value)
 
-@app.route("/get-foodie-data", methods=['POST'])
-def getFoodieData():
-    FoodieID = request.json['CustID']
-    response = RESTAURANTS.order_by_child('CustID').equal_to(FoodieID).get()
-    for key, value in response.items():
-        print(value)
-    return(value)
+@app.route("/add-favorite", methods=['POST'])
+def addFavorite():
+    favorite_chef = request.json['ChefID']
+    foodieID = request.json['CustID']
+    foodie = FOODIES.order_by_child('CustID').equal_to(foodieID).get()
+    print(foodie)
+    foodie['1']['Favorites'].append(favorite_chef)
+    FOODIES.update({
+        '1': foodie['1']
+    })
+    # print("running add favorite")
+    # print(type(FOODIES.child))
+    return favorite_chef
+
 
 ########### END OF FOODIE FUNCTIONS ###########
 
@@ -119,4 +138,3 @@ def createMatch():
     return create_match
 
 ########### END OF MISC FUNCTIONS ###########
-
--- backEnd/test.py
+++ backEnd/test.py
@@ -4,16 +4,12 @@ import json
 url1 = "http://127.0.0.1:5000/get-restaurant-data"
 url2 = "http://127.0.0.1:5000/get-chef-data"
 url3 = "http://127.0.0.1:5000/get-foodie-data"
-<<<<<<< HEAD
 match_url = "http://127.0.0.1:5000/create-match"
-=======
->>>>>>> e09d4c2d1e0e0c2584e59f3a39415ba7b3f58eca
 
 payload1 = {
     "RestID": "Rest001",
 }
 
-<<<<<<< HEAD
 payload2 = {
     "ChefID": "Chef001",
 }
@@ -43,16 +39,4 @@ print(res2.content)
 print("\n")
 print(res3.content)
 
-=======
-payload1 = {
-    "CustID": "Cust001",
-}
-
-payload3 = {
-    "ChefID": "Chef001",
-}
-
-
-res = requests.post(url3,data=json.dumps(payload1), headers={'Content-Type' : 'application/json'})
->>>>>>> e09d4c2d1e0e0c2584e59f3a39415ba7b3f58eca
 
